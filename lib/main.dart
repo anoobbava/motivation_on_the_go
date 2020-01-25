befcec1b83f755cd368f1f7964b678e671ec2c5b
Anoob Bava <anoob.bava@gmail.com>
2020-01-25 10:47:46 +0530

    Added the dependent packages for API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import './Quote.dart';
 
 void main() => runApp(
       MaterialApp(
