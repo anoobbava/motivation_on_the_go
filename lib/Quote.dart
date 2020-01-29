6e73fcc4769eb193ad0157c3231708208f2f8e6e
Anoob Bava <anoob.bava@gmail.com>
2020-01-29 18:24:44 +0530

    Added the Quote class with parsing from sqflite

--- lib/Quote.dart
+++ lib/Quote.dart
@@ -1,14 +1,27 @@
 class Quote {
-  final String quoteText;
-  final String quoteDate;
-  final String quoteAuthor;
-  Quote({this.quoteText, this.quoteDate, this.quoteAuthor});
+  int quoteId;
+  String quoteText;
+  String quoteAuthor;
+  Quote({this.quoteId, this.quoteText, this.quoteAuthor});
 
   factory Quote.fromJson(Map<String, dynamic> json) {
     return Quote(
       quoteText: json['quote']['body'],
-      quoteDate: json['qotd_date'],
       quoteAuthor: json['quote']['author'],
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': quoteId,
+      'quote_text': quoteText,
+      'quote_author': quoteAuthor,
+    };
+  }
+
+  Quote.fromMap(Map<String, dynamic> map) {
+    quoteId = map['id'];
+    quoteText = map['quote_text'];
+    quoteAuthor = map['quote_author'];
   }
 }
